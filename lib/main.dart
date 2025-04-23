e8d4b8d5416a28c5b61cfaa4e0d099c08e5ba46f
AkhrievSuleym <murid2005@mail.ru>
2025-04-24 02:46:28 +0300

    add firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_chat_app/colors.dart';
+import 'package:my_chat_app/firebase_options.dart';
 import 'package:my_chat_app/screens/mobile_layout_screen.dart';
 import 'package:my_chat_app/screens/web_layout_screen.dart';
 import 'package:my_chat_app/utils/responsive_layout.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
