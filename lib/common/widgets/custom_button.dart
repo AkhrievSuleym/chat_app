cba83849556bb24c26360202a04562b8ea7a9b09
AkhrievSuleym <murid2005@mail.ru>
2025-04-28 01:48:55 +0300

    some changes in welcome_screen

---
+++ lib/common/widgets/custom_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:my_chat_app/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: tabColor,
+        minimumSize: const Size(double.infinity, 50),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.lightGreen[50],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/landing/screens/welcome_screen.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:my_chat_app/colors.dart';
+import 'package:my_chat_app/common/widgets/custom_button.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/welcome_screen.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Center(
+              child: Column(
+                children: [
+                  const SizedBox(height: 50),
+                  const Text(
+                    'Welcome!',
+                    style: TextStyle(
+                      fontFamily: 'BigShouldersStencil',
+                      fontSize: 55,
+                      fontWeight: FontWeight.bold,
+                      color: Colors
+                          .white, // Белый текст для лучшей читаемости на фоне
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10,
+                          color: Colors.black,
+                          offset: Offset(2, 2),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Spacer(),
+                  const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'Read our Privacy Policy. Tap "Agree and continue" to accept the Terms of Service.',
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 216, 215, 215)),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: size.width * 0.75,
+                    child: CustomButton(
+                      text: 'AGREE AND CONTINUE',
+                      onPressed: () {},
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
