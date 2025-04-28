ff90a4ddf1b9e3f9cead3311bb5d443523bb0675
AkhrievSuleym <murid2005@mail.ru>
2025-04-29 02:33:58 +0300

    end with login page

--- lib/features/auth/pages/login_page.dart
+++ lib/features/auth/pages/login_page.dart
@@ -59,7 +59,7 @@ class _LoginPageState extends State<LoginPage> {
               height: 10,
             ),
             TextButton(
-              onPressed: () {},
+              onPressed: () => pickCountry,
               child: const Text('Pick Number'),
             ),
             const SizedBox(
