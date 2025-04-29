9af98cdda35fba75ad4f2d996261f80aad31f11f
AkhrievSuleym <murid2005@mail.ru>
2025-04-30 00:12:33 +0300

    add signUp

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:my_chat_app/common/widgets/error.dart';
 import 'package:my_chat_app/features/auth/pages/login_page.dart';
+import 'package:my_chat_app/features/auth/pages/otp_page.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case LoginPage.routeName:
       return MaterialPageRoute(
         builder: (context) => const LoginPage(),
+      );
+    case OTPPage.routeName:
+      return MaterialPageRoute(
+        builder: (context) => const OTPPage(),
       );
     default:
       return MaterialPageRoute(
